e21e060be934a59e56387ce63aa9208feb4f274a
ivan <ivan.trabucco@yahoo.it>
2020-03-11 22:42:44 +0100

    footer WIP 2

--- app/Components/Footer/FooterComponent.js
+++ app/Components/Footer/FooterComponent.js
@@ -1,47 +1,47 @@
 import React from "react";
+import {COL_3} from "../../Core/Costants/CentralContentConstants";
 
 export const FooterComponent = ({cssClass = ""}) => {
 
     return <div className="footer-container">
+        <div className="footer-section-container">
 
-        <div className={"footer-section " +  cssClass}>
-            <ul>
-                <li>-footer 1-</li>
-                <li>-footer 2-</li>
-                <li>-footer 3-</li>
-                <li>-footer 4-</li>
-                <li>-footer 5-</li>
-                <li>-footer 6-</li>
-                <li>-footer 7-</li>
-            </ul>
-        </div>
+            <div className={"footer-section " + cssClass}>
+                <ul>
+                    <li>-footer 1-</li>
+                    <li>-footer 2-</li>
+                    <li>-footer 3-</li>
+                    <li>-footer 4-</li>
+                    <li>-footer 5-</li>
+                    <li>-footer 6-</li>
+                    <li>-footer 7-</li>
+                </ul>
+            </div>
+            <div className={ cssClass === COL_3 ?  "footer-vertical-line" : "footer-horizontal-line" }/>
+            <div className={"footer-section " + cssClass}>
+                <ul>
+                    <li>-footer 1-</li>
+                    <li>-footer 2-</li>
+                    <li>-footer 3-</li>
+                    <li>-footer 4-</li>
+                    <li>-footer 5-</li>
+                    <li>-footer 6-</li>
+                    <li>-footer 7-</li>
+                </ul>
 
-        <div className="footer-vertical-line"/>
-
-        <div className={"footer-section " +  cssClass}>
-            <ul>
-                <li>-footer 1-</li>
-                <li>-footer 2-</li>
-                <li>-footer 3-</li>
-                <li>-footer 4-</li>
-                <li>-footer 5-</li>
-                <li>-footer 6-</li>
-                <li>-footer 7-</li>
-            </ul>
-        </div>
-
-        <div className="footer-vertical-line"/>
-
-        <div className={"footer-section " +  cssClass}>
-            <ul>
-                <li>-footer 1-</li>
-                <li>-footer 2-</li>
-                <li>-footer 3-</li>
-                <li>-footer 4-</li>
-                <li>-footer 5-</li>
-                <li>-footer 6-</li>
-                <li>-footer 7-</li>
-            </ul>
+            </div>
+            <div className={ cssClass === COL_3 ?  "footer-vertical-line" : "footer-horizontal-line" }/>
+            <div className={"footer-section " + cssClass}>
+                <ul>
+                    <li>-footer 1-</li>
+                    <li>-footer 2-</li>
+                    <li>-footer 3-</li>
+                    <li>-footer 4-</li>
+                    <li>-footer 5-</li>
+                    <li>-footer 6-</li>
+                    <li>-footer 7-</li>
+                </ul>
+            </div>
         </div>
     </div>
 };
