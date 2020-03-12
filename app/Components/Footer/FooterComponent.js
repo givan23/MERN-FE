2054d1cc0bee4503cc7e94725ff61aa4ca68b5ce
ivan <ivan.trabucco@yahoo.it>
2020-03-12 01:42:13 +0100

    footer WIP 3

--- app/Components/Footer/FooterComponent.js
+++ app/Components/Footer/FooterComponent.js
@@ -1,47 +1,47 @@
 import React from "react";
 import {COL_3} from "../../Core/Costants/CentralContentConstants";
 
-export const FooterComponent = ({cssClass = ""}) => {
+export const FooterComponent = ({cssClass = "", firstSectionList, secondSectionList, thirdSectionList, numberSections}) => {
 
     return <div className="footer-container">
         <div className="footer-section-container">
 
             <div className={"footer-section " + cssClass}>
                 <ul>
-                    <li>-footer 1-</li>
-                    <li>-footer 2-</li>
-                    <li>-footer 3-</li>
-                    <li>-footer 4-</li>
-                    <li>-footer 5-</li>
-                    <li>-footer 6-</li>
-                    <li>-footer 7-</li>
+                    <SectionDetail SectionList={firstSectionList}/>
                 </ul>
             </div>
-            <div className={ cssClass === COL_3 ?  "footer-vertical-line" : "footer-horizontal-line" }/>
+
+            <DivideSectionLineComponent cssClass={cssClass}/>
+
             <div className={"footer-section " + cssClass}>
                 <ul>
-                    <li>-footer 1-</li>
-                    <li>-footer 2-</li>
-                    <li>-footer 3-</li>
-                    <li>-footer 4-</li>
-                    <li>-footer 5-</li>
-                    <li>-footer 6-</li>
-                    <li>-footer 7-</li>
+                    <SectionDetail SectionList={secondSectionList}/>
                 </ul>
 
             </div>
-            <div className={ cssClass === COL_3 ?  "footer-vertical-line" : "footer-horizontal-line" }/>
+
+            <DivideSectionLineComponent cssClass={cssClass}/>
+
             <div className={"footer-section " + cssClass}>
                 <ul>
-                    <li>-footer 1-</li>
-                    <li>-footer 2-</li>
-                    <li>-footer 3-</li>
-                    <li>-footer 4-</li>
-                    <li>-footer 5-</li>
-                    <li>-footer 6-</li>
-                    <li>-footer 7-</li>
+                    <SectionDetail SectionList={thirdSectionList}/>
                 </ul>
             </div>
         </div>
     </div>
+};
+
+
+const SectionDetail = ({SectionList = []})=> {
+
+    return SectionList.map((item,i) => {
+        return <li key={i}><div>{item.detail}</div></li>
+    });
+};
+
+const DivideSectionLineComponent = ({cssClass})=> {
+
+    return <div className={ cssClass === COL_3 ?  "footer-vertical-line" : "footer-horizontal-line" }/>
+
 };
